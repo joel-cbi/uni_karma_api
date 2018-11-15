e40b744861ece6eab228a71d51d105ac3f433016
Albert Yin <ayin@cbinsights.com>
2018-11-15 15:21:20 -0500

    show_karma is a post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :histories
   resources :users
   post '/give_karma', to: 'karma#give_karma', as: 'give_karma'
-  get '/show_karma', to: 'karma#show_karma', as: 'show_karma'
+  post '/show_karma', to: 'karma#show_karma', as: 'show_karma'
   post '/show_leaderboard', to: 'karma#show_leaderboard', as: 'show_leaderboard'
   post '/show_friends', to: 'karma#show_friends', as: 'show_friends'
   post '/show_foes', to: 'karma#show_foes', as: 'show_foes'
