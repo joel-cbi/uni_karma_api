f459f20476d3f5dc3949d0e4bcf1d69b5969d55a
Albert Yin <ayin@cbinsights.com>
2018-11-15 15:31:00 -0500

    fix syntax

--- app/controllers/karma_controller.rb
+++ app/controllers/karma_controller.rb
@@ -16,7 +16,7 @@ class KarmaController < ApplicationController
   def show_karma
     if authenticate
       @karma_value = User.all.select(:slack_id, :karma)
-      @texts "You have #{@karma_value} karma"
+      @texts = "You have #{@karma_value} karma"
       render json: {"text": @texts, "mrkdwn": true, "response_type": "in_channel"}
     else
       render json: {message: 'Not Authorized. API_TOKEN missing'}, status: 401
